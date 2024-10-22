f327b3e2567c95b1f642b1328b043eea85421d6c
Meghana <meg.karthic@gmail.com>
2024-10-22 14:19:49 -0400

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/jstelnik/Team-MONA/",
   plugins: [react()],
   resolve: {
     alias: {
@@ -11,3 +12,4 @@ export default defineConfig({
     },
   },
 });
+
